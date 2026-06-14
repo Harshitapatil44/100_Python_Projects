96d80d7e81a0891499b0c0da1c40470ed154f79e
harshita Patil <patilharshita347@gmail.com>
2026-06-14 15:23:30 +0530

    Update 29. Pomodoro Timer.py

--- 29. Pomodoro Timer.py
+++ 29. Pomodoro Timer.py
@@ -1,1 +1,24 @@
+import time
 
+work = int(input("Enter work time (minutes): "))
+break_time = int(input("Enter break time (minutes): "))
+
+print("\nWork Session Started")
+
+for i in range(work * 60, 0, -1):
+    mins = i // 60
+    secs = i % 60
+    print(f"\r{mins:02}:{secs:02}", end="")
+    time.sleep(1)
+
+print("\nWork Session Complete!")
+
+print("Break Started")
+
+for i in range(break_time * 60, 0, -1):
+    mins = i // 60
+    secs = i % 60
+    print(f"\r{mins:02}:{secs:02}", end="")
+    time.sleep(1)
+
+print("\nBreak Complete!")
