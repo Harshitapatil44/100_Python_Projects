ae2e5e3207ac2db2e9387d98a55fe6aa872c169a
harshita Patil <patilharshita347@gmail.com>
2026-07-11 16:32:31 +0530

    Update 41. Recipe Recommander  system.py

--- 41. Recipe Recommander  system.py
+++ 41. Recipe Recommander  system.py
@@ -1,1 +1,23 @@
+# Simple Recipe Recommender System
 
+recipes = {
+    "Pasta": ["pasta", "tomato", "cheese"],
+    "Vegetable Sandwich": ["bread", "tomato", "cucumber", "cheese"],
+    "Paneer Curry": ["paneer", "onion", "tomato"],
+    "Fruit Salad": ["apple", "banana", "orange"],
+    "Omelette": ["egg", "onion", "cheese"]
+}
+
+ingredient = input("Enter an ingredient: ").lower()
+
+found = False
+
+print("\nRecommended Recipes:")
+
+for recipe, ingredients in recipes.items():
+    if ingredient in ingredients:
+        print("-", recipe)
+        found = True
+
+if not found:
+    print("No recipes found with that ingredient.")
