1d4354b223bb0845cb7b5e5ed5d352705768f43c
harshita Patil <patilharshita347@gmail.com>
2026-07-22 22:48:59 +0530

    Update 47. Word crumble Game.py

--- 47. Word crumble Game.py
+++ 47. Word crumble Game.py
@@ -1,1 +1,20 @@
+import random
 
+words = ["python", "apple", "banana", "computer", "school", "laptop"]
+
+word = random.choice(words)
+
+letters = list(word)
+random.shuffle(letters)
+scrambled = "".join(letters)
+
+print("=== Word Scramble Game ===")
+print("Unscramble the word:", scrambled)
+
+guess = input("Enter your guess: ").lower()
+
+if guess == word:
+    print("🎉 Correct! You Win!")
+else:
+    print("❌ Wrong!")
+    print("The correct word was:", word)
