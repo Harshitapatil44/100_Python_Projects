47956597d4aaabf294d459309a17b06fb03d9b29
harshita Patil <patilharshita347@gmail.com>
2026-07-23 23:19:11 +0530

    Update 48. Word Guess game.py

--- 48. Word Guess game.py
+++ 48. Word Guess game.py
@@ -1,1 +1,29 @@
+import random
 
+words = ["apple", "python", "banana", "laptop", "school", "flower"]
+
+word = random.choice(words)
+guessed = ["_"] * len(word)
+attempts = 6
+
+print("=== Word Guess Game ===")
+
+while attempts > 0 and "_" in guessed:
+    print("\nWord:", " ".join(guessed))
+    guess = input("Enter a letter: ").lower()
+
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessed[i] = guess
+        print("Correct!")
+    else:
+        attempts -= 1
+        print("Wrong! Attempts left:", attempts)
+
+if "_" not in guessed:
+    print("\n🎉 You Win!")
+    print("The word was:", word)
+else:
+    print("\n😢 You Lose!")
+    print("The word was:", word)
