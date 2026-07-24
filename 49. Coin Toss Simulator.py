fc4a6f838283a552294a384596f59b0b63b40c4b
harshita Patil <patilharshita347@gmail.com>
2026-07-24 23:39:03 +0530

    Update 49. Coin Toss Simulator.py

--- 49. Coin Toss Simulator.py
+++ 49. Coin Toss Simulator.py
@@ -1,1 +1,13 @@
+import random
 
+print("=== Coin Toss Simulator ===")
+
+while True:
+    toss = random.choice(["Heads", "Tails"])
+    print("Result:", toss)
+
+    again = input("Toss again? (yes/no): ").lower()
+
+    if again != "yes":
+        print("Thanks for playing!")
+        break
