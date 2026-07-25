4da6eb27aa1d60ae0f63c03d1502e150f96a0b41
harshita Patil <patilharshita347@gmail.com>
2026-07-25 23:52:31 +0530

    Update 50. Lottery Number generator.py

--- 50. Lottery Number generator.py
+++ 50. Lottery Number generator.py
@@ -1,1 +1,8 @@
+import random
 
+tickets = int(input("How many tickets do you want? "))
+
+for i in range(tickets):
+    numbers = random.sample(range(1, 50), 6)
+    numbers.sort()
+    print(f"Ticket {i + 1}: {numbers}")
