4a9739859381fe5c41ad3b4a5bebf4fe20a73e13
harshita Patil <patilharshita347@gmail.com>
2026-07-28 21:26:07 +0530

    Update 51. Bingo Number Game.py

--- 51. Bingo Number Game.py
+++ 51. Bingo Number Game.py
@@ -1,1 +1,12 @@
+import random
 
+print("🎉 Welcome to Bingo Number Generator 🎉")
+
+numbers = list(range(1, 91))
+random.shuffle(numbers)
+
+while numbers:
+    input("\nPress Enter to draw a number...")
+    print("Number:", numbers.pop())
+
+print("\nAll bingo numbers have been drawn!")
